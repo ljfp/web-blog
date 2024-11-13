4cafe6ce5409c6324e2421b054da7b094d5bb52d
LJFP <ljfp@ljfp.xyz>
2024-11-13 17:16:41 +0000

    Use dark-grey variant of the theme.

--- conf.py
+++ conf.py
@@ -1425,7 +1425,7 @@ WARN_ABOUT_TAG_METADATA = False
 # Put in global_context things you want available on all your templates.
 # It can be anything, data, functions, modules, etc.
 GLOBAL_CONTEXT = {}
-GLOBAL_CONTEXT["HACK_VARIANT"] = "dark"
+GLOBAL_CONTEXT["HACK_VARIANT"] = "dark-grey"
 
 # Add functions here and they will be called with template
 # GLOBAL_CONTEXT as parameter when the template is about to be
